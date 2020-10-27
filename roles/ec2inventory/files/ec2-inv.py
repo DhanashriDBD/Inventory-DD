2c744b1307909efd2c3ffa3faff069364aadf961
root <root@ip-172-31-21-214.us-east-2.compute.internal>
2020-10-27 12:06:00 +0000

    Your Message

--- roles/ec2inventory/files/ec2-inv.py
+++ roles/ec2inventory/files/ec2-inv.py
@@ -9,35 +9,35 @@ all_regions = ec2_cli.describe_regions()
 #print(all_regions['Regions'])
 
 #CSV creation and write data to CSV
-csv_ob=open("EC2-Inventory.csv","w",newline='')
-csv_w=csv.writer(csv_ob)
-csv_w.writerow(['Instance ID','Region','Instance Type','State','Private IP','Public IP','Vpc ID','Subnet ID','Account ID'])
+with open("EC2-Inventory.csv","wb") as csv_ob:
+  csv_w=csv.writer(csv_ob)
+  csv_w.writerow(['Instance ID','Region','Instance Type','State','Private IP','Public IP','Vpc ID','Subnet ID','Account ID'])
 #destionation = '/home/centos/inventory'
 
 #Fetching AWS regional list from AWS
-list_regions = []
-for each_reg in all_regions['Regions']:
-    #print(each_reg['RegionName'])
-    list_regions.append(each_reg['RegionName'])
+  list_regions = []
+  for each_reg in all_regions['Regions']:
+      #print(each_reg['RegionName'])
+      list_regions.append(each_reg['RegionName'])
 #print(list_regions)
 
-count=1
-for each_reg in list_regions:
-    ec2_cli = boto3.client('ec2',region_name=each_reg)
-    response=ec2_cli.describe_instances()['Reservations']
-    for each_item in response:
-        for instances in each_item['Instances']:
-            if instances['State']['Name']== 'running' or instances['State']['Name']== 'stopped':
-                ipaddress = instances.get('PublicIpAddress')
-                if ipaddress is None:
-                    ipaddress = 'NA'
-                    csv_w.writerow([instances['InstanceId'],instances['Placement']['AvailabilityZone'],
-                    instances['InstanceType'],instances['State']['Name'],instances['PrivateIpAddress'],ipaddress,
-                    instances['VpcId'],instances['SubnetId'],each_item['OwnerId']])
-                else:
-                    if ipaddress is not None:
-                        csv_w.writerow([instances['InstanceId'],instances['Placement']['AvailabilityZone'],
-                        instances['InstanceType'],instances['State']['Name'],instances['PrivateIpAddress'],ipaddress,
-                        instances['VpcId'],instances['SubnetId'],each_item['OwnerId']])
-csv_ob.close()
+  count=1
+  for each_reg in list_regions:
+      ec2_cli = boto3.client('ec2',region_name=each_reg)
+      response=ec2_cli.describe_instances()['Reservations']
+      for each_item in response:
+          for instances in each_item['Instances']:
+              if instances['State']['Name']== 'running' or instances['State']['Name']== 'stopped':
+                  ipaddress = instances.get('PublicIpAddress')
+                  if ipaddress is None:
+                      ipaddress = 'NA'
+                      csv_w.writerow([instances['InstanceId'],instances['Placement']['AvailabilityZone'],
+                      instances['InstanceType'],instances['State']['Name'],instances['PrivateIpAddress'],ipaddress,
+                      instances['VpcId'],instances['SubnetId'],each_item['OwnerId']])
+                  else:
+                      if ipaddress is not None:
+                          csv_w.writerow([instances['InstanceId'],instances['Placement']['AvailabilityZone'],
+                          instances['InstanceType'],instances['State']['Name'],instances['PrivateIpAddress'],ipaddress,
+                          instances['VpcId'],instances['SubnetId'],each_item['OwnerId']])
+  csv_ob.close()
 #shutil.move(csv.w,destination)
